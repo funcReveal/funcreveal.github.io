74c903721a9a6a7fbb886559199c680bdebf2ea7
zhenyu <zrecordy.1003@gmail.com>
2025-04-28 20:57:30 +0800

    Initial deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  trailingSlash: true,
-  basePath: isProd ? "/funcreveal.github.io" : "", // 例如 /funcReveal
-  assetPrefix: isProd ? "/funcreveal.github.io/" : "", // 讓 static 資源也吃正確路徑
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
