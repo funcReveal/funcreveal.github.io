d7babcab918a5abb98b62b73d7940ed40bc2ae84
zhenyu <zrecordy.1003@gmail.com>
2025-04-28 18:01:04 +0800

    modify

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   trailingSlash: true,
   basePath: isProd ? "/funcreveal.github.io" : "", // 例如 /funcReveal
   assetPrefix: isProd ? "/funcreveal.github.io/" : "", // 讓 static 資源也吃正確路徑
